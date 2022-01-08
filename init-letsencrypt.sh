60c2094fa7b234bd103f6048113f114155e01006
matter <bti@btcpayprovider.com>
2022-01-07 21:11:00 -0600

    New email
    Change email

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=($DOMAIN)
 rsa_key_size=4096
 data_path="./config/certbot"
-email="adam@coinos.io" # Adding a valid address is strongly recommended
+email="info@kisi.app" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
